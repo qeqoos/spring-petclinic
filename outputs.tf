45174d2d08ab5f0391d9fe84ed7750e59e2f6c61
Pavel Patalashko <foxthesaw@gmail.com>
2021-10-24 17:17:13 +0300

    activated all 3 instances

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "Jenkins-master-public-IP" {
   value = aws_instance.jenkins-master.public_ip
 }
 
-# output "Jenkins-worker-public-IP" {
-#   value = aws_instance.jenkins-worker.public_ip
-# }
+output "Jenkins-worker-public-IP" {
+  value = aws_instance.jenkins-worker.public_ip
+}
 
 output "qa-instance-public-IP" {
   value = aws_instance.qa_instance.public_ip
